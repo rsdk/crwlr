c8d3fd6aed4e04df3325112594830fd9764a0677
rsdk <rnkblr@gmail.com>
2013-08-07 19:18:20 +0200

    Kommentare aktualisiert und aufgeräumt

--- crwl.go
+++ crwl.go
@@ -14,9 +14,9 @@ import (
 	"time"
 )
 
-const MaxOutstanding_URL = 100
-const MaxOutstanding_RESP = 4
-const DEBUG = 1
+const MaxOutstanding_Fetcher = 100 //Maximale Anzahl gleichzeitger Fetcher
+const MaxOutstanding_Parser = 4    //Maximale Anzahl gleichzeitiger Parser
+const DEBUG = 1                    //Debugausgabe an/aus
 
 type URLINDEX struct {
 	URL   string
@@ -28,57 +28,52 @@ type HTTPRESP struct {
 	FD  io.Reader
 }
 
-//globaler URL Speicher - um doppeltes crwln zu vermeiden
-var crwldurls map[string]bool
+var crwldurls map[string]bool // globale URL Map - um doppeltes HTTP GET zu vermeiden
 
 //Channels
 var chan_urls = make(chan string, 100000)      // buffered channel of strings
 var chan_ioreaders = make(chan HTTPRESP, 200)  // buffered channel of structs
 var chan_urlindexes = make(chan URLINDEX, 100) // buffered channel of structs
+
 //Semaphore Channels
-var sem_URL = make(chan int, MaxOutstanding_URL)
-var sem_RESP = make(chan int, MaxOutstanding_RESP)
+var sem_Fetcher = make(chan int, MaxOutstanding_Fetcher)
+var sem_Parser = make(chan int, MaxOutstanding_Parser)
 
 func debugausgabe(msg string) {
 	if DEBUG == 1 {
-
 		fmt.Printf("%s DEBUG: %s\n", time.Now(), msg)
 	}
 }
 
 func handleFetcher(url string) {
-	//debugausgabe("Fetcher starten")
-	<-sem_URL     // Wait for active queue to drain.
-	fetchURL(url) // May take a long time.
-	sem_URL <- 1  // Done; enable next request to run.
+	<-sem_Fetcher // Eine Ressource verbrauchen: Lock falls bereits alle verbraucht
+	fetchURL(url)
+	sem_Fetcher <- 1 // Eine Ressource wieder freigeben
 }
 
 func handleParser(a HTTPRESP) {
-	//debugausgabe("Parser starten")
-	<-sem_RESP    // Wait for active queue to drain.
-	parseHtml(a)  // May take a long time.
-	sem_RESP <- 1 // Done; enable next request to run.
+	<-sem_Parser // Eine Ressource verbrauchen: Lock falls bereits alle verbraucht
+	parseHtml(a)
+	sem_Parser <- 1 // Eine Ressource wieder freigeben
 }
 
 func starten() {
-	//Initiales setzen der Ressourcen
-	for i := 0; i < MaxOutstanding_URL; i++ {
-		sem_URL <- 1
+	// Initiales setzen der Ressourcen
+	for i := 0; i < MaxOutstanding_Fetcher; i++ {
+		sem_Fetcher <- 1
 	}
-	//Initiales setzen der Ressourcen
-	for i := 0; i < MaxOutstanding_RESP; i++ {
-		sem_RESP <- 1
+	// Initiales setzen der Ressourcen
+	for i := 0; i < MaxOutstanding_Parser; i++ {
+		sem_Parser <- 1
 	}
 	for {
-		work := <-chan_urls
-		go handleFetcher(work) // Don't wait for handle to finish.
-		work2 := <-chan_ioreaders
-		go handleParser(work2)
+		go handleFetcher(<-chan_urls)
+		go handleParser(<-chan_ioreaders)
 	}
 }
 
 // parseHTML bekommt eine komplette HTML Seite
-// und gibt je eine Map mit Links und Wörtern zurück
+// und legt eine Map mit Wörtern und (viele) einzelne Links in entsprechende Channels
 func parseHtml(a HTTPRESP) {
 	//start := time.Now()
 	d := html.NewTokenizer(a.FD)
@@ -88,26 +83,24 @@ func parseHtml(a HTTPRESP) {
 	for {
 		// token type
 		tokenType := d.Next()
-		// if d.Err() != nil {
-		// 	fmt.Printf("TokenERR vorher: %v", d.Err())
-		// }
-
+		// ErrorToken kommt (auch) beim Ende der Daten
 		if tokenType == html.ErrorToken {
-			chan_urlindexes <- URLINDEX{a.URL, words}
+			chan_urlindexes <- URLINDEX{a.URL, words} // WORD-Map in den Channel legen
 			//fmt.Printf("Parse-Dauer : [%.2fs]  URL: %s\n", time.Since(start).Seconds(), a.URL)
 			return
 		}
 		token := d.Token()
 		switch tokenType {
 		case html.StartTagToken: // <tag>
-			//Eine Map mit Links erstellen
+			// Links finden
 			if token.Data == "a" {
 				for _, element := range token.Attr {
 					if element.Key == "href" {
-						//Link normalisieren
+						// Link normalisieren
 						url, err := url.Parse(element.Val)
+						// Nur Absolute Links die nicht in der globalen Link Map sind
 						if url.IsAbs() && err == nil && url.Scheme == "http" && crwldurls[url.String()] != true {
-							chan_urls <- url.String()
+							chan_urls <- url.String() // Die URL in den Channel legen
 						}
 					}
 				}
@@ -115,7 +108,7 @@ func parseHtml(a HTTPRESP) {
 
 		case html.TextToken: // text between start and end tag
 			//Map mit Wörtern erstellen
-			temp := strings.Fields(token.Data)
+			temp := strings.Fields(token.Data) //Aufteilen in Einzelne Wörter, trennen bei Whitespace
 			for _, element := range temp {
 				//TODO: einzelne Örter noch besser von Sonderzeichen trennen z.b. mit TRIM()
 				words[element] = words[element] + 1
@@ -131,7 +124,7 @@ func parseHtml(a HTTPRESP) {
 
 // fetchURL Bekommt eine URL
 // lädt die Seite herunter
-// Gibt die komplette Seite zurück
+// und legt ein struct vom Typ HTTPRESP in den Channel chan_ioreaders
 func fetchURL(url string) {
 	//start := time.Now()
 	crwldurls[url] = true //URL in die globale URL Liste aufnehmen damit sie nicht nochmal in den Work Queue kommt.
@@ -146,7 +139,7 @@ func fetchURL(url string) {
 	return
 }
 
-//save holt sich daten (structs vom Typ urlindexes) aus dem Channel chan_urlindexes
+//save holt sich daten (structs vom Typ URLINDEX) aus dem Channel chan_urlindexes
 // und schreibt die Daten in eine Datei "output.txt"
 func save() {
 
@@ -170,7 +163,7 @@ func save() {
 	}
 }
 
-//writeDB holt sich daten (structs vom Typ urlindexes) aus dem Channel chan_urlindexes
+//writeDB holt sich daten (structs vom Typ URLINDEX) aus dem Channel chan_urlindexes
 // und schreibt die Daten in eine sqlite3 Datenbank: crwld.db
 func writeDB() {
 	os.Remove("./crwld.db")
@@ -182,6 +175,7 @@ func writeDB() {
 	}
 	defer db.Close()
 
+	//Neue Tabelle(n) erstellen
 	sqls := []string{
 		"create table data (url text not null, word text, count int)",
 		"delete from data",
@@ -196,8 +190,7 @@ func writeDB() {
 
 	// Endlosworker
 	for {
-		//Holt sich neue Arbeit aus dem Channel
-		index := <-chan_urlindexes
+		index := <-chan_urlindexes //Neue Arbeit aus dem Channel holen
 		debugausgabe(index.URL)
 
 		tx, err := db.Begin()
@@ -206,13 +199,14 @@ func writeDB() {
 			return
 		}
 
-		stmt, err := tx.Prepare("insert into data(url, word, count) values(?, ?, ?)")
+		stmt, err := tx.Prepare("insert into data(url, word, count) values(?, ?, ?)") //Maske für das SQL Statement setzen
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 		defer stmt.Close()
 
+		// Die gesamte MAP durchlaufen und für jeden Key ein SQl Statement zusammensetzen
 		for element := range index.WORDS {
 			_, err = stmt.Exec(index.URL, element, index.WORDS[element])
 			if err != nil {
@@ -227,10 +221,10 @@ func writeDB() {
 func main() {
 	starturl := "http://www.htw-aalen.de" //Start URL festlegen
 
-	chan_urls <- starturl //Ersten URL in Channel
+	chan_urls <- starturl //URL in den Channel legen
 	crwldurls = make(map[string]bool)
-	debugausgabe("Starte DB Writer")
 	//go save() //File Writer starten
+	debugausgabe("Starte DB Writer")
 	go writeDB() //DB Writer starten
 	debugausgabe("Starte Crawler")
 	starten() //Crawler starten
